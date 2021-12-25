057fb4ea02b2db91c46a2241351694996e06bab7
Matej Arlović <matej.arlovic@gmail.com>
2021-12-25 11:47:10 +0100

    Added frame searching and snapshoting them (backwards).

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import vlc
 import keyboard
 import time
+import math
 
 from modules.stream_provider import *
-from modules.callbacks import *
+
+def mspf(mp):
+    """Milliseconds per frame"""
+    return int(1000 // (mp.get_fps() or 25))
 
 instance = vlc.Instance()
 provider = StreamProvider('videos', 'mp4', instance)
@@ -17,12 +21,21 @@ while True:
     try:
         if keyboard.is_pressed('q'):
             #print(player.get_position())
-            print(player.get_fps())
-            player.set_position(0.013967541977763176)
             time.sleep(0.08)
             player.pause()
+            time.sleep(0.08)
 
-            player.video_take_snapshot(0, 'screenshots', 0, 0)
+            current_frame = player.get_time() / mspf(player)
+            for i in range(math.ceil(player.get_fps())):
+                print(i, math.ceil((current_frame - i) * mspf(player)))
+                player.set_time(math.ceil((current_frame - i) * mspf(player)))
+                time.sleep(0.25)
+                player.video_take_snapshot(0, 'screenshots', 1920, 1080)
+
+            #player.set_time(13401)
+            #player.set_position(0.013967541977763176)
+            time.sleep(0.1)
+            #player.video_take_snapshot(0, 'screenshots', 0, 0)
             break
     except:
         break
